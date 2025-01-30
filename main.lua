e09d3f47818f6d2a7438838b1d83902be528f2c1
Alex <awei799331@gmail.com>
2025-01-30 02:36:48 -0800

    Hubris, fix tour guide

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 --- MOD_ID: BetterBalancedBalatro
 --- MOD_AUTHOR: [108_Pho, Akrone]
 --- MOD_DESCRIPTION: Additional archetype support for enhanced cards, and hand types
---- PREFIX: bbb
+--- PREFIX: betterbalancedbalatro
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
@@ -147,6 +147,13 @@ SMODS.Joker{
   end,
   
   calculate = function(self,card,context)
+    if context.setting_blind and not self.getting_sliced and context.cardarea == G.jokers then
+      juice_card_until(
+        card,
+        (function() return context.setting_blind and G.GAME.current_round.hands_played == 0 end),
+        true,
+        nil)
+    end
     if context.cardarea == G.jokers and context.before then
       if #context.full_hand == 1 and (context.full_hand[1]:get_id() == 11 or context.full_hand[1]:get_id() == 12 or context.full_hand[1]:get_id() == 13) and G.GAME.current_round.hands_played == 0 then
         ease_dollars(5, true)
@@ -157,10 +164,9 @@ SMODS.Joker{
       end
     end
     if context.destroy_card and context.cardarea == G.play then
-      sendDebugMessage('ya mum', 'robes')
       if #context.full_hand == 1 and (context.full_hand[1]:get_id() == 11 or context.full_hand[1]:get_id() == 12 or context.full_hand[1]:get_id() == 13) and G.GAME.current_round.hands_played == 0 then
         return {
-         remove = true
+          remove = true
         }
       end
     end
@@ -392,7 +398,7 @@ SMODS.Joker{
         -- message = "X"..card.ability.extra.Xmult,
         colour = G.C.MULT,
       }
-    elseif context.setting_blind and G.GAME.blind:get_type() ~= 'Boss' then
+    elseif context.setting_blind and not self.getting_sliced and G.GAME.blind:get_type() ~= 'Boss' then
       card.ability.extra.Xmult = math.max(1.0, card.ability.extra.Xmult - card.ability.extra.minus)
       return {
         card = card,
@@ -412,8 +418,82 @@ SMODS.Joker{
   loc_txt = { -- local text
       name = 'Tour Guide',
       text = {
-        'Sell this joker after skipping 3 blinds for',
-        '{C:attention}-1{} Ante'
+        'Sell this joker after',
+        'skipping #2# blinds for',
+        '{C:attention}-1{} Ante',
+        'Currently: {C:attention}#1#{} skips'
+      },
+      --[[unlock = {
+          'Be {C:legendary}cool{}',
+      }]]
+  },
+  atlas = 'Jokers', --atlas' key
+  rarity = 2, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+  --soul_pos = { x = 0, y = 0 },
+  cost = 5, --cost
+  unlocked = true, --where it is unlocked or not: if true, 
+  discovered = true, --whether or not it starts discovered
+  blueprint_compat = false, --can it be blueprinted/brainstormed/other
+  eternal_compat = false, --can it be eternal
+  perishable_compat = true, --can it be perishable
+  pos = {x = 5, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+  config = { 
+    extra = {
+      skipped_blinds = 0,
+      required_skips = 3
+    }
+  },
+  set_ability = function(self, card, initial, delay_sprites)
+    card.ability.skipped_blinds = 0
+  end,
+  loc_vars = function(self,info_queue,center)
+    return {vars = {center.ability.extra.skipped_blinds, center.ability.extra.required_skips}}
+  end,
+  check_for_unlock = function(self, args)
+    return true
+  end,
+  calculate = function(self,card,context)
+    if context.skip_blind then
+      card.ability.extra.skipped_blinds = card.ability.extra.skipped_blinds + 1
+      if card.ability.extra.skipped_blinds >= self.config.extra.required_skips then
+        juice_card_until(card, (function() return true end), true, 1.5)
+        return {
+          card = card,
+          message = 'Ready!',
+          colour = G.C.GOLD
+        }
+      end
+      return {
+        card = card,
+        message = 'Skipped!',
+        colour = G.C.GOLD
+      }
+    end
+    if context.selling_self then
+      if card.ability.extra.skipped_blinds >= self.config.extra.required_skips then
+        ease_ante(-1)
+        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
+        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante - 1
+        G.HUD:recalculate()
+      end
+    end
+  end,
+  in_pool = function(self,args)
+    --whether or not this card is in the pool, return true if it is, return false if its not
+    return G.GAME.round_resets.ante >= 3
+  end,
+}
+
+SMODS.Joker{
+  key = 'hubris', --joker key
+  loc_txt = { -- local text
+      name = 'Hubris',
+      text = {
+        'Lose all hands except 1.',
+        'Gain {X:mult,C:white}X#1#{} Mult',
+        'for each hand lost.',
+        'Gain an additional {X:mult,C:white}X#1#{}',
+        'if you own {C:attention}Vanity{}'
       },
       --[[unlock = {
           'Be {C:legendary}cool{}',
@@ -431,36 +511,36 @@ SMODS.Joker{
   pos = {x = 5, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
   config = { 
     extra = {
-      skipped_blinds = 0,
-      
+      Xmult = 1.0,
+      currentXmult = 1.0
     }
   },
   loc_vars = function(self,info_queue,center)
-    return {vars = {center.ability.extra.Xmult, center.ability.extra.minus}} --#1# is replaced with card.ability.extra.Xmult
+    return {vars = {center.ability.extra.Xmult}}
   end,
   check_for_unlock = function(self, args)
     return true
   end,
   calculate = function(self,card,context)
-    if context.joker_main then
+    if context.setting_blind and not self.getting_sliced then
+      card.ability.extra.currentXmult = 1.0
+      local current_hands = G.GAME.current_round.hands_left
+      local hands_to_cut = current_hands - 1
+      card.ability.extra.currentXmult = card.ability.extra.currentXmult + hands_to_cut
+      ease_hands_played(-hands_to_cut)
       return {
-        card = card,
-        xmult = card.ability.extra.Xmult,
-        -- message = "X"..card.ability.extra.Xmult,
-        colour = G.C.MULT,
+        message = '-'..hands_to_cut..' Hands',
+        colour = G.C.IMPORTANT
       }
-    elseif context.setting_blind and G.GAME.blind:get_type() ~= 'Boss' then
-      card.ability.extra.Xmult = math.max(1.0, card.ability.extra.Xmult - card.ability.extra.minus)
+
+    elseif context.joker_main then
       return {
-        card = card,
-        message = "-"..card.ability.extra.minus,
-        colour = G.C.RED
+        xmult = card.ability.extra.currentXmult
       }
     end
   end,
   in_pool = function(self,args)
-    --whether or not this card is in the pool, return true if it is, return false if its not
-    return G.GAME.round_resets.ante <= 4
+    return true
   end,
 }
 
