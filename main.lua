1bf12acffc43e518aa24f51fe2a98e3330a99f5b
SpacyAkrone <jackson.ribble@gmail.com>
2025-01-30 14:51:14 -0500

    fixed egg text and message (#2)
    * ya mum
    
    * fixed egg text and message

--- main.lua
+++ main.lua
@@ -183,7 +183,7 @@ SMODS.Joker{
   loc_txt = { -- local text
       name = 'Fried Egg',
       text = {
-        'After three rounds, this egg hatches and ',
+        'After {C:attention}3{} rounds, this egg hatches and ',
         'Golden Goose may appear in the shop',
         'Played rounds: {C:blue}#1#{}'
       },
@@ -220,12 +220,19 @@ SMODS.Joker{
   end,
   calculate = function(self,card,context)
     if context.end_of_round and context.cardarea == G.jokers then
-      sendDebugMessage("Ran calculate", "Fried Egg")
       card.ability.extra.played_rounds = card.ability.extra.played_rounds + 1
       if card.ability.extra.played_rounds >= card.ability.extra.hatch_time then
         G.GAME.pool_flags.fried_egg_hatched = true
-        card:remove()
-        return
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, func = function()
+          card:remove()
+          return true
+        end}))
+      end
+      if G.GAME.pool_flags.fried_egg_hatched == true then
+        return {
+          message = 'Hatched!',
+          colour = G.C.GOLD
+        }
       end
     end
   end,
@@ -544,6 +551,44 @@ SMODS.Joker{
   end,
 }
 
+SMODS.Joker{
+  key = 'showboat', --joker key
+  loc_txt = { -- local text
+      name = 'Showboat',
+      text = {
+        'Earn {C:gold}$7{}',
+        'at end of round if',
+        '{C:attention}Blind{} was defeated',
+        'with one hand'
+      },
+      --[[unlock = {
+          'Be {C:legendary}cool{}',
+      }]]
+  },
+  atlas = 'Jokers', --atlas' key
+  rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+  --soul_pos = { x = 0, y = 0 },
+  cost = 6, --cost
+  unlocked = true, --where it is unlocked or not: if true, 
+  discovered = true, --whether or not it starts discovered
+  blueprint_compat = true, --can it be blueprinted/brainstormed/other
+  eternal_compat = true, --can it be eternal
+  perishable_compat = true, --can it be perishable
+  pos = {x = 7, y = 1}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+  config = { 
+    extra = {
+    }
+  },
+  check_for_unlock = function(self, args)
+      return true
+  end,
+  
+  calc_dollar_bonus = function(self, card)
+    if G.GAME.current_round.hands_played == 1 then
+      return 7
+    end
+  end,
+}
 
 SMODS.Joker{
   key = 'emme_joker', --joker key
