4d3c231699875cbb9e6986484a6d725878164855
SpacyAkrone <jackson.ribble@gmail.com>
2025-01-31 21:42:01 -0500

    final fix hopefully (#5)

---
+++ src/joker/aaajokertemplate.lua
@@ -0,0 +1,42 @@
+SMODS.Joker{
+    key = "yourkeyhere", --joker key
+    loc_txt = { -- local text
+        name = "yournamehere",
+        text = {
+          "Your joker text here bucko"
+        },
+    },
+    atlas = "Jokers", --atlas" key
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 4, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 4, y = 4}, --position in atlas, starts at 0, scales by the atlas" card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+        filler = 0
+      }
+    },
+  
+    loc_vars = function(self,info_queue,center)
+      return {vars = {center.ability.extra.filler}} --Replace 'filler'
+    end,
+  
+    check_for_unlock = function(self, args)
+        return true
+    end,
+  
+    calculate = function(self,card,context)
+      -- TODO
+    end,
+  
+    in_pool = function(self,args)
+      return true
+    end
+  
+  }
+  
